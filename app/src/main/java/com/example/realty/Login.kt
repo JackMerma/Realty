928f0b2d0ce5956ba3efdb1c3d25ed0b885c4b1f
AxelAlmonte <aalmontecu@unsa.edu.pe>
2024-11-01 01:00:32 -0500

    implementacion de Login

--- app/src/main/java/com/example/realty/Login.kt
+++ app/src/main/java/com/example/realty/Login.kt
@@ -1,16 +1,38 @@
 package com.example.realty
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.realty.ui.theme.RealtyTheme
 
 class Login : ComponentActivity() {
@@ -19,11 +41,10 @@ class Login : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RealtyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +52,129 @@ class Login : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.Gray) // Fondo genérico, puedes colocar una imagen de fondo si la tienes
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fondo), // Coloca tu imagen de fondo
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(Color(0x80000000)) // Fondo semi-transparente
+        ) {
+            Text(
+                text = "Inicio de Sesion",
+                fontSize = 32.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Ingrese sus Credenciales",
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = "", onValueChange = {},
+                label = { Text("Correo") },
+                leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
+                keyboardOptions = KeyboardOptions.Default.copy(autoCorrect = false),
+                modifier = Modifier.fillMaxWidth()
+                    .background(color = Color.White)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = "", onValueChange = {},
+                label = { Text("Contraseña") },
+                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = null) },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+                    .background(color = Color.White)
+            )
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { /* Acción de registro */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
+            ) {
+                Text("Iniciar Sesion", color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.apple),
+                    contentDescription = "Apple Icon",
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clickable { /* Acción con Apple */ }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.facebook),
+                    contentDescription = "Facebook Icon",
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clickable { /* Acción con Facebook */ }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.google_ic),
+                    contentDescription = "Google Icon",
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clickable { /* Acción con Google */ }
+                )
+            }
+
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("¿No tienes cuenta?", color = Color.White)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Registrate",
+                    color = Color.Green,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable { /* Acción de inicio de sesión */ }
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     RealtyTheme {
-        Greeting2("Android")
+        LoginScreen()
     }
 }
