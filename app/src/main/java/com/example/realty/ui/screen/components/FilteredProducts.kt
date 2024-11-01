35be9bfbdbacba4d63707899c5e040a4ead34679
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-11-01 18:08:17 -0500

    feat: defining a rating bar

--- app/src/main/java/com/example/realty/ui/screen/components/FilteredProducts.kt
+++ app/src/main/java/com/example/realty/ui/screen/components/FilteredProducts.kt
@@ -3,20 +3,30 @@ package com.example.realty.ui.screen.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.realty.data.Product
 
 @Composable
 fun FilteredProducts(
@@ -47,5 +57,22 @@ fun FilterOptions() {
         Text(text = "Popularity", modifier = Modifier.clickable { /* Do nothing for now */ })
         Text(text = "Newest", modifier = Modifier.clickable { /* Do nothing for now */ })
         Text(text = "Most Expensive", modifier = Modifier.clickable { /* Do nothing for now */ })
+    }
+
+@Composable
+fun RatingBar(rating: Float, reviews: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        /*
+        Icon(
+            painter = painterResource(id = R.drawable.ic_star),
+            contentDescription = "Rating",
+            tint = Color(0xFFFFD700),
+            modifier = Modifier.size(16.dp)
+        )
+        */
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = "$rating", fontSize = 14.sp)
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = "($reviews Reviews)", fontSize = 12.sp, color = Color.Gray)
     }
 }
