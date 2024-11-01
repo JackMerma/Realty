a4e7d2b28baee2384879b1bce98422df215f3716
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-11-01 18:01:59 -0500

    feat: implementing a composable Header

--- app/src/main/java/com/example/realty/ui/screen/components/FilteredProducts.kt
+++ app/src/main/java/com/example/realty/ui/screen/components/FilteredProducts.kt
@@ -1,8 +1,16 @@
 package com.example.realty.ui.screen.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FilteredProducts(
@@ -13,4 +21,10 @@ fun FilteredProducts(
         text = "Search -> $query",
         modifier = modifier
     )
+@Composable
+fun Header() {
+    Column {
+        Text(text = "Headphone", fontSize = 16.sp, color = Color.Gray)
+        Text(text = "TMA Wireless", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+    }
 }
